0bb987d9cdd1cb2c46e9da1793e3db3466b024c6
Lorwik <manueljsandalio@gmail.com>
2023-03-05 11:55:06 +0100

    Comando !neko

---
+++ src/comandos/anime/neko.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    DESCRIPTION: "Te enviaré una imagen de una neko al azar.",
+    ALIASES: ["waifu"],
+
+    execute: async (client, message, args, GUILD_DATA, prefix) => {
+
+    // Enviar una solicitud HTTP para obtener una imagen SFW de neko
+    const response = await fetch(`https://api.waifu.pics/sfw/neko`);
+    const data = await response.json();
+
+    // Comprobar si se encontró una imagen
+    if (data.url) {
+      // Crear un mensaje con la imagen
+        const Embed = new EmbedBuilder()
+        .setImage(data.url)
+        .setColor(process.env.COLOR)
+        .setTimestamp();
+
+        message.reply({ embeds: [Embed] })
+
+    } else {
+      // Si no se encontró una imagen, enviar un mensaje de error
+      message.reply(`No se encontró una imagen para la categoría neko.`);
+    }
+  }
+}
