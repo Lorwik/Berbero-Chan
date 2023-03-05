3ca07ef359eeaf2075f4de3bd2d64012651a5e15
Lorwik <manueljsandalio@gmail.com>
2023-03-05 12:00:11 +0100

    Comando NSFW Neko

---
+++ src/comandos/nsfw/nsfwneko.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    DESCRIPTION: "Envia una imagen NSFW aleatoria de una neko 🔞",
+    ALIASES: ["trampa"],
+
+    execute: async (client, message, args, GUILD_DATA, prefix) => {
+
+    // Enviar una solicitud HTTP para obtener una imagen NSFW de neko
+    const response = await fetch(`https://api.waifu.pics/nsfw/neko`);
+    const data = await response.json();
+
+    // Comprobar si se encontró una imagen
+    if (data.url) {
+      // Crear un mensaje con la imagen
+        const Embed = new EmbedBuilder()
+        .setImage(data.url)
+        .setColor(process.env.COLOR)
+        .setTimestamp();
+
+        message.reply({ embeds: [Embed] })
+
+    } else {
+      // Si no se encontró una imagen, enviar un mensaje de error
+      message.reply(`No se encontró una imagen para la categoría neko.`);
+    }
+  }
+}
