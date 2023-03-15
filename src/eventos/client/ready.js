0aa6bc2c85d3aa4881da2e2ad4cb7ad20b1397e0
Lorwik <manueljsandalio@gmail.com>
2023-03-15 13:27:44 +0100

    Agregado soporte para bases de dato MongoDB

--- src/eventos/client/ready.js
+++ src/eventos/client/ready.js
@@ -1,7 +1,30 @@
-module.exports = client => {
-    console.log(`Sesión iniciada como ${client.user.tag}`.green);
+const mongoose = require('mongoose');
 
-    if(client?.application?.commands){
+module.exports = client => {
+    let palito = 53;
+
+    mongoose.connect(process.env.MONGODB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log(`
+╔═════════════════════════════════════════════════════╗
+║                                                     ║
+║       Conectado a la base de datos de MONGODB!      ║
+║                                                     ║
+╚═════════════════════════════════════════════════════╝`.blue)
+    }).catch((err) => {
+        console.log(`☁ ERROR AL CONECTAR A LA BASE DE DATOS DE MONGODB`.red);
+        console.log(err)
+    })
+
+    console.log(`╔═════════════════════════════════════════════════════╗`.green)
+    console.log(`║ `.green + " ".repeat(-1 + palito - 1) + " ║".green)
+    console.log(`║ `.green + `      Conectado como ${client.user.tag}`.green + " ".repeat(-1 + palito - 1 - `      Conectado como ${client.user.tag}`.length) + " ║".green)
+    console.log(`║ `.green + " ".repeat(-1 + palito - 1) + " ║".green)
+    console.log(`╚═════════════════════════════════════════════════════╝`.green)
+
+    if (client?.application?.commands) {
         client.application.commands.set(client.slashArray);
         console.log(`(/) ${client.slashCommands.size} Comandos Publicados!`.green)
     }
--- src/eventos/guild/messageCreate.js
+++ src/eventos/guild/messageCreate.js
@@ -1,32 +1,47 @@
+const Discord = require('discord.js');
+const serverSchema = require(`${process.cwd()}/src/models/servidor.js`)
+
 module.exports = async (client, message) => {
 
-    if(!message.guild || !message.channel || message.author.bot) return;
+    if (!message.guild || !message.channel || message.author.bot) return;
 
-    if(!message.content.startsWith(process.env.PREFIX)) return;
+    let data = await serverSchema.findOne({guildID: message.guild.id});
+    
+    //si el bot es mencionado, devolvemos un mensaje de respuesta indicando el prefijo establecido en el servidor
+    if (message.content.includes(client.user.id)) return message.reply({
+        embeds: [
+            new Discord.EmbedBuilder()
+                .setTitle(`✅ **Para la lista de comandos usa \`${data.prefijo}help\`!**`)
+                .setFooter({ text: `© desarrollado por Lorwik | 2023`, iconURL: `https://cdn.discordapp.com/avatars/679037019837562908/baf4ebdefcee53573bfb48be20e0fb41.webp?size=1024` })
+                .setColor(process.env.COLOR)
+        ]
+    })
+
+    if (!message.content.startsWith(process.env.PREFIX)) return;
 
     const ARGS = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
     const CMD = ARGS?.shift()?.toLowerCase();
 
     const COMANDO = client.commands.get(CMD) || client.commands.find(c => c.ALIASES && c.ALIASES.includes(CMD));
 
-    if(COMANDO){
-        if(COMANDO.OWNER) {
+    if (COMANDO) {
+        if (COMANDO.OWNER) {
             const DUENOS = process.env.OWNER_IDS.split(" ");
-            if(!DUENOS.includes(message.author.id)) return message.reply({content: `❌ **Solo los dueños de este bot pueden ejecutar este comando **\nDueños del bot: ${DUENOS.map(DUENO => `<@${DUENO}>`).join(", ")}`})
+            if (!DUENOS.includes(message.author.id)) return message.reply({ content: `❌ **Solo los dueños de este bot pueden ejecutar este comando **\nDueños del bot: ${DUENOS.map(DUENO => `<@${DUENO}>`).join(", ")}` })
         }
 
-        if(COMANDO.BOT_PERMISSIONS) {
-            if(!message.guild.members.me.permissions.has(COMANDO.BOT_PERMISION)) return message.reply({content: `❌ **Necesito los siguientes permisos para ejecutar este comando **\n${COMANDO.BOT_PERMISIONS.map(PERMISO => `\`${PERMISO}\``).join(", ")}`})
+        if (COMANDO.BOT_PERMISSIONS) {
+            if (!message.guild.members.me.permissions.has(COMANDO.BOT_PERMISION)) return message.reply({ content: `❌ **Necesito los siguientes permisos para ejecutar este comando **\n${COMANDO.BOT_PERMISIONS.map(PERMISO => `\`${PERMISO}\``).join(", ")}` })
         }
-        
-        if(COMANDO.PERMISSIONS) {
-            if(!message.members.permissions.has(COMANDO.PERMISION)) return message.reply({content: `❌ **Necesitas los siguientes permisos para ejecutar este comando **\n${COMANDO.BOT_PERMISIONS.map(PERMISO => `\`${PERMISO}\``).join(", ")}`})
+
+        if (COMANDO.PERMISSIONS) {
+            if (!message.members.permissions.has(COMANDO.PERMISION)) return message.reply({ content: `❌ **Necesitas los siguientes permisos para ejecutar este comando **\n${COMANDO.BOT_PERMISIONS.map(PERMISO => `\`${PERMISO}\``).join(", ")}` })
         }
 
         try {
             COMANDO.execute(client, message, ARGS, process.env.PREFIX);
         } catch (e) {
-            message.reply({content: ' **Ha ocurrido un error al ejecutar el comando.**\n'});
+            message.reply({ content: ' **Ha ocurrido un error al ejecutar el comando.**\n' });
             console.log(e);
             return;
         }
